cbe6b7a1e5b090e14b911d4c628d37a2a2529951
Emmanuel Chinonye Nnajiofor <emma.nnajichi@gmail.com>
2024-04-30 15:10:48 +0100

    added minor text to questions on covid lab

--- src/routes/DiseaseSpecific/Covid19/LaboratoryInformation/index.js
+++ src/routes/DiseaseSpecific/Covid19/LaboratoryInformation/index.js
@@ -204,7 +204,7 @@ const LaboratoryInformation = ({ form }) => {
                         collectFormName={true}
                         setFormValues={setFormValues}
                         form={form}
-                        label="Nasal swab"
+                        label="Nasal swab received"
                         name="nSwabSpecimenReceived"
                         labelCol={{ span: 24 }}
                         wrapperCol={{ span: 24 }}
@@ -239,7 +239,7 @@ const LaboratoryInformation = ({ form }) => {
                         collectFormName={true}
                         setFormValues={setFormValues}
                         form={form}
-                        label="Throat swab"
+                        label="Throat swab received"
                         name="tSwabSpecimenReceived"
                         labelCol={{ span: 24 }}
                         wrapperCol={{ span: 24 }}
@@ -274,7 +274,7 @@ const LaboratoryInformation = ({ form }) => {
                         collectFormName={true}
                         setFormValues={setFormValues}
                         form={form}
-                        label="Nasopharyngeal swab"
+                        label="Nasopharyngeal swab received"
                         name="npSwabSpecimenReceived"
                         labelCol={{ span: 24 }}
                         wrapperCol={{ span: 24 }}
